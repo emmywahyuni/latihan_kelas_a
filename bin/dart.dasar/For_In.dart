51a2a3087bb49e3ed53913d4efda9416db0a2c10
emmywahyuni <emmyun2401@gmail.com>
2024-10-07 20:34:17 +0000

    For_In.dart

---
+++ bin/dart.dasar/For_In.dart
@@ -0,0 +1,26 @@
+void main(List<String> args) {
+  //Perulangan For in digunakan untuk mengakses seluruh data di List secara otomatis
+
+  //Tanpa for in
+  // var names = <String>['Emmy', 'Wahyuni',];
+
+  // for (var i = 0; i < names.length; i++) {
+  //   print(names[i]);
+  // }
+
+  //Menggunakan For in bisa digunakan untuk list dan set
+  var names = <String>['Emmy', 'Wahyuni'];
+
+  for (var value in names) {
+    print(value);
+  }
+
+  //Gunakan set
+  var nameSet = <String>{
+    'Emmy',
+    'Wahyuni',
+  };
+  for (var value in nameSet) {
+    print(value);
+  }
+}
