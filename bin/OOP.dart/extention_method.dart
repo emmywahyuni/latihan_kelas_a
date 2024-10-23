ba61b9d4b03fd98f61a9e14f195d381c00106c0b
emmywahyuni <emmyun2401@gmail.com>
2024-10-23 23:02:30 +0000

    RABU

---
+++ bin/OOP.dart/extention_method.dart
@@ -0,0 +1,49 @@
+class Person {
+  //Atribut atau Field
+  String name = "Emmy"; //Non nullable
+  String? address; //bisa null
+  final String country =
+      "Indonesia"; //FInal -> tidak bisa mengubah data fieldnya
+
+  //Method
+  void sayHello(String paramName) {
+    print('Hello $paramName, My Name Is $name');
+  }
+
+  void hello() {
+    print("Hello, my name is $name");
+  }
+
+  String getName() {
+    return "Hello, my name is $name";
+  }
+}
+
+//Extension Method , ingin method tambahan tanpa mengubah isi dari class nya bisa gunakan extension method
+extension SayGoodByeOnPerson on Person {
+  void sayGoodBye(String paramName) {
+    print("Good Bye $paramName, from $name");
+  }
+}
+
+void main() {
+  var person1 = Person(); //Objek Pertama
+  //Manipulasi Field
+  person1.name = "Emmy Wahyu Ni";
+  person1.address = "Banjarmasin";
+  //person1.country = "Singapore"; //Final -> tidak bisa mengubah data fieldnya
+
+  //Menampilkan data
+  print(person1.name);
+  print(person1.address);
+  print(person1.country);
+  //Memanggil Method
+  person1.sayHello("Emmy");
+  person1.hello();
+
+  //Memanggil Extension Method
+  person1.sayGoodBye("Wahyu");
+
+  Person person2 = Person(); //Objek Kedua
+  print(person2);
+}
